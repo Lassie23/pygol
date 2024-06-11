7c40648b33e0a07b350a8bb81b9321ff0edd5e9c
Lassie23 <67132399+Lassie23@users.noreply.github.com>
2024-06-11 12:32:38 +1000

    Make use of actual coordinates

--- gol.py
+++ gol.py
@@ -41,94 +41,56 @@ def makescreen():
     screen = pygame.display.set_mode((bx*res, by*res))
     board = []
     sboard = []
-    for z in range(0,bx*by):
-        board.append(0)
-        sboard.append(0)
+    for x in range(0,bx):
+        board.append([])
+        sboard.append([])
+        for y in range(0,by):
+            board[x].append(0)
+            sboard[x].append(0)
 getconfig()
 makescreen()
 upd = False
 def updateboard():
     x = -1
-    for z in range(0,bx*by):
-        sboard[z] = 0
-    for y in board:
+    for a in range(0,bx):
+        for b in range(0,by):
+            sboard[a][b] = 0
+    for a in board:
         x += 1
-        uedge = False
-        dedge = False
-        ledge = False
-        redge = False
-        if x % bx == 0:
-            ledge = True
-        elif x % bx == bx - 1:
-            redge = True
-        if x < bx:
-            uedge = True
-        elif x > bx * by - bx - 1:
-            dedge = True
-        if ledge == False:
-            sboard[x] += board[x-1]
-        else:
-            sboard[x] += board[x-1+bx]
-        if redge == False:
-            sboard[x] += board[x+1]
-        else:
-            sboard[x] += board[x+1-bx]
-        if uedge == False:
-            sboard[x] += board[x-bx]
-            if ledge == False:
-                sboard[x] += board[x-bx-1]
-            else:
-                sboard[x] += board[x-1]
-            if redge == False:
-                sboard[x] += board[x-bx+1]
-            else:
-                sboard[x] += board[x-bx-bx+1]
-        else:
-            sboard[x] += board[x-bx+bx*by]
-            if ledge == False:
-                sboard[x] += board[x-bx-1+bx*by]
-            else:
-                sboard[x] += board[x-1+bx*by]
-            if redge == False:
-                sboard[x] += board[x-bx+1+bx*by]
-            else:
-                sboard[x] += board[x-bx-bx+1+bx*by]
-        if dedge == 0:
-            sboard[x] += board[x+bx]
-            if ledge == 0:
-                sboard[x] += board[x+bx-1]
-            else:
-                sboard[x] += board[x+bx+bx-1]
-            if redge == 0:
-                sboard[x] += board[x+bx+1]
-            else:
-                sboard[x] += board[x+1]
-        else:
-            sboard[x] += board[x+bx-bx*by]
-            if ledge == 0:
-                sboard[x] += board[x+bx-1-bx*by]
-            else:
-                sboard[x] += board[x+bx+bx-1-bx*by]
-            if redge == 0:
-                sboard[x] += board[x+bx+1-bx*by]
-            else:
-                sboard[x] += board[x+1-bx*by]
+        y = -1
+        for b in a:
+            y += 1
+            sboard[x][y] += \
+            board[(x-1)%bx][(y-1)%by] + \
+            board[(x-1)%bx][y] + \
+            board[(x-1)%bx][(y+1)%by] + \
+            board[x][(y-1)%by] + \
+            board[x][(y+1)%by] + \
+            board[(x+1)%bx][(y-1)%by] + \
+            board[(x+1)%bx][y] + \
+            board[(x+1)%bx][(y+1)%by]
     x = -1
-    for y in board:
+    for a in board:
         x += 1
-        if board[x] == 1:
-            if not str(sboard[x]) in [*survival]:
-                board[x] = 0
-        else:
-            if str(sboard[x]) in birth:
-                board[x] = 1
+        y = -1
+        for b in a:
+            y += 1
+            if board[x][y] == 1:
+                if not str(sboard[x][y]) in [*survival]:
+                    board[x][y] = 0
+            else:
+                if str(sboard[x][y]) in birth:
+                    board[x][y] = 1
 def draw():
     screen.fill((0,0,0))
     x = -1
-    for y in board:
+    for a in board:
         x += 1
-        if y == 1:
-            screen.fill((255,255,255),(x % bx * res,x // bx * res,res,res))
+        y = -1
+        for b in a:
+            y += 1
+            if b == 1:
+                screen.fill((255,255,255),(x * res,y * res,res,res))
     for x in range(1,bx):
         screen.fill((128,128,128),(x*res,0,grl,by*res))
     for y in range(1,by):
@@ -141,18 +103,21 @@ while True:
             if event.__dict__["text"] == " ":
                 upd = True
             elif event.__dict__["text"] == "r":
-                for z in range(0,bx*by):
-                    board[z] = 0
+                for a in range(0,bx):
+                    for b in range(0,by):
+                        board[a][b] = 0
             elif event.__dict__["text"] == "c":
                 pygame.display.quit()
                 getconfig()
                 makescreen()
             draw()
         if event.type == 1025:
-            if board[(event.__dict__["pos"][0]//res)+(event.__dict__["pos"][1]//res*bx)] == 0:
-                board[(event.__dict__["pos"][0]//res)+(event.__dict__["pos"][1]//res*bx)] = 1
+            x = event.__dict__["pos"][0]//res
+            y = event.__dict__["pos"][1]//res
+            if board[x][y] == 0:
+                board[x][y] = 1
             else:
-                board[(event.__dict__["pos"][0]//res)+(event.__dict__["pos"][1]//res*bx)] = 0
+                board[x][y] = 0
             draw()
         elif event.type == pygame.QUIT:
             quit()
